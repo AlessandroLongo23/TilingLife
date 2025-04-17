ae0b145e6934a5cc5c1dc9cb746c8292c755b7c0
Alberto Basaglia <alberto.basaglia17@gmail.com>
2025-04-17 10:22:44 +0200

    add custom rule

--- src/serverless/main.py
+++ src/serverless/main.py
@@ -3,15 +3,16 @@ import numpy as np
 from tqdm import tqdm
 from dataclasses import dataclass
 from typing import List
+import time
 
 show_gui = False
 
-ti.init(arch=ti.gpu, default_fp=ti.f32, default_ip=ti.i32, kernel_profiler=True)
+ti.init(arch=ti.gpu, default_fp=ti.f32, default_ip=ti.i32, kernel_profiler=True, random_seed=int(time.time()))
 
 n = 256
 alive_p = 0.15
 iterations = 1000
-random_starts = 100
+random_starts = 5
 
 current = ti.field(dtype=ti.i32, shape=(n, n))
 next = ti.field(dtype=ti.i32, shape=(n, n))
@@ -21,28 +22,51 @@ pixels = ti.field(dtype=ti.f32, shape=(n, n))
 alive_count = ti.field(dtype=ti.i32, shape=()) # Total number of alive cells in the current frame
 population_sum = ti.field(dtype=ti.i32, shape=()) # Total number of alive cells in the simulation since the start
 
+births_sum = ti.field(dtype=ti.i32, shape=()) # Total number of births in the simulation since the start
+deaths_sum = ti.field(dtype=ti.i32, shape=()) # Total number of deaths in the simulation since the start
+
+alive = ti.field(dtype=ti.i32, shape=(n, n))
+
+current_birth_rule = ti.field(dtype=ti.i8, shape=(9))
+current_survive_rule = ti.field(dtype=ti.i8, shape=(9))
+
+
 @dataclass
 class StartMetrics:
     average_population: float
+    acivity: float
 
 @dataclass
 class RuleMetrics:
     average_population: float
+    activity: float
 
     @staticmethod
     def from_starts(starts: List[StartMetrics]):
+
+        # Calculate the average population and activity across all starts
         average_population = 0.0
         for start in starts:
             average_population += start.average_population
         average_population /= len(starts)
 
-        return RuleMetrics(average_population=average_population)
+        # Calculate the average activity across all starts
+        activity = 0.0
+        for start in starts:
+            activity += start.acivity
+        activity /= len(starts)
+
+
+        return RuleMetrics(
+            average_population=average_population,
+            activity=activity
+        )
 
 
 @ti.kernel
 def init():
     for i, j in current:
-        if ti.random() < 0.15:
+        if ti.random() < alive_p:
             current[i, j] = 1
         else:
             current[i, j] = 0
@@ -59,7 +83,7 @@ def count_neighbors(i, j):
     return count
 
 @ti.func
-def compute_metrics():
+def update_metrics():
     alive_count[None] = 0
     for i, j in current:
         if current[i, j] == 1:
@@ -70,29 +94,46 @@ def compute_metrics():
 
 
 
-def summary_metrics() -> StartMetrics:
+def summarize_metrics() -> StartMetrics:
     average_population = population_sum[None]
     average_population /= iterations
     average_population /= (n * n)
 
-    return StartMetrics(average_population=average_population)
+    activity = births_sum[None] + deaths_sum[None]
+    activity /= iterations
+    activity /= (n * n)
+
+    return StartMetrics(
+        average_population=average_population,
+        acivity=activity
+    )
+
+@ti.func
+def should_birth(count):
+    return current_birth_rule[count] == 1
+
+@ti.func
+def should_survive(count):
+    return current_survive_rule[count] == 1
 
 
 @ti.kernel
 def update_swap():
 
-    compute_metrics()
+    update_metrics()
 
     for i, j in next:
         neighbors = count_neighbors(i, j)
         if current[i, j] == 1:
-            if neighbors < 2 or neighbors > 3:
-                next[i, j] = 0
+            if should_survive(neighbors):
+                next[i, j] = 1
             else:
-                next[i, j] = 1
+                next[i, j] = 0
+                deaths_sum[None] += 1
         else:
-            if neighbors == 3:
+            if should_birth(neighbors):
                 next[i, j] = 1
+                births_sum[None] += 1
             else:
                 next[i, j] = 0
     
@@ -114,6 +155,8 @@ gui = ti.GUI("Game of Life", (n, n))
 def test_start() -> StartMetrics:
     alive_count[None] = 0
     population_sum[None] = 0
+    births_sum[None] = 0
+    deaths_sum[None] = 0
 
     init()
     for step in range(iterations):
@@ -124,7 +167,7 @@ def test_start() -> StartMetrics:
 
         update_swap()
     
-    return summary_metrics()
+    return summarize_metrics()
 
 
 def test_rule() -> RuleMetrics:
@@ -132,14 +175,24 @@ def test_rule() -> RuleMetrics:
     for i in range(random_starts):
         metrics = test_start()
         metrics_list.append(metrics)
+        print(metrics)
     
     return RuleMetrics.from_starts(metrics_list)
+    
 
 def main():
+    current_birth_rule.fill(0)
+    current_birth_rule[3] = 1
+    current_survive_rule.fill(0)
+    current_survive_rule[2] = 1
+    current_survive_rule[3] = 1
+
     rule_metrics = test_rule()
 
     print("Final Rule Metrics:")
     print(f"Average population: {rule_metrics.average_population:.4f}")
+    print(f"Activity: {rule_metrics.activity:.4f}")
+
 
 
 if __name__ == "__main__":
